8026168bfcd7cbd46ffe98770f02b191e05ee2e2
Shagnik Sarkar <shagniksarkar07@gmail.com>
2025-07-21 11:54:40 +0530

    Delete cursor.js

--- cursor.js
+++
@@ -1,8 +0,0 @@
-const cursor = document.querySelector('.custom-cursor');
-document.addEventListener('mousemove', (e) => {
-  cursor.style.transform = `translate(${e.clientX - 9}px, ${e.clientY - 9}px)`;
-});
-document.addEventListener('mousedown', () => {
-  cursor.style.transform += ' scale(1.3)';
-  setTimeout(() => cursor.style.transform = cursor.style.transform.replace(' scale(1.3)', ''), 140);
-});
